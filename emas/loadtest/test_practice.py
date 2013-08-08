947ccafeb04fb2d0a9856fef4159c6544ed6e8af
Rijk Stofberg <rijk@upfrontsystems.co.za>
2013-08-08 15:12:33 +0200

    Fix group name

--- emas/loadtest/test_practice.py
+++ emas/loadtest/test_practice.py
@@ -19,19 +19,19 @@ class practice(FunkLoadTestCase):
         credential_port = self.conf_getInt('credential', 'port')
         self.login, self.password = xmlrpc_get_credential(credential_host,
                                                           credential_port,
-                                                          'members')
+                                                          'funkloadgroup')
 
     def test_PracticeSystem(self):
         # The description should be set in the configuration file
         server_url = self.server_url
         # begin of test ---------------------------------------------
 
-        self.get("http://qa.everythingmaths.co.za/",
+        self.get("http://qap.everythingmaths.co.za/",
             description="Get /")
-        self.get("http://qa.everythingmaths.co.za/login?ajax_load=1335182298146",
+        self.get("http://qap.everythingmaths.co.za/login?ajax_load=1335182298146",
             description="Get /login")
-        self.post("http://qa.everythingmaths.co.za/login_form", params=[
-            ['came_from', 'http://qa.everythingmaths.co.za/'],
+        self.post("http://qap.everythingmaths.co.za/login_form", params=[
+            ['came_from', 'http://qap.everythingmaths.co.za/'],
             ['next', ''],
             ['ajax_load', '1335182298146'],
             ['ajax_include_head', ''],
@@ -47,99 +47,99 @@ class practice(FunkLoadTestCase):
             ['__ac_password', self.password],
             ['submit', 'Log in']],
             description="Post /login_form")
-        self.get("http://qa.everythingmaths.co.za/",
+        self.get("http://qap.everythingmaths.co.za/",
             description="Get /")
-        self.get("http://qa.everythingmaths.co.za/@@practice",
+        self.get("http://qap.everythingmaths.co.za/@@practice",
             description="Get /@@practice")
-        self.get("http://qa.everythingmaths.co.za/@@practice/static/thumbs_up_24.png%27;",
-            description="Get /@@practice/static/thumbs_up_24.png%27;")
-        self.get("http://qa.everythingmaths.co.za/@@practice/static/please_wait_24.gif%27;",
-            description="Get /@@practice/static/please_wait_24.gif%27;")
-        self.get("http://qa.everythingmaths.co.za/acl_users/credentials_cookie_auth/require_login?came_from=http%3A//qa.everythingmaths.co.za/%40%40practice/static/thumbs_up_24.png%2527%253B",
+        self.get("http://qap.everythingmaths.co.za/@@practice/static/thumbs_up_24.png;",
+            description="Get /@@practice/static/thumbs_up_24.png;")
+        self.get("http://qap.everythingmaths.co.za/@@practice/static/please_wait_24.gif;",
+            description="Get /@@practice/static/please_wait_24.gif;")
+        self.get("http://qap.everythingmaths.co.za/acl_users/credentials_cookie_auth/require_login?came_from=http%3A//qap.everythingmaths.co.za/%40%40practice/static/thumbs_up_24.png",
             description="Get /acl_users/credenti..._auth/require_login")
-        self.get("http://qa.everythingmaths.co.za/acl_users/credentials_cookie_auth/require_login?came_from=http%3A//qa.everythingmaths.co.za/%40%40practice/static/please_wait_24.gif%2527%253B",
+        self.get("http://qap.everythingmaths.co.za/acl_users/credentials_cookie_auth/require_login?came_from=http%3A//qap.everythingmaths.co.za/%40%40practice/static/please_wait_24.gif",
             description="Get /acl_users/credenti..._auth/require_login")
-        self.post("http://qa.everythingmaths.co.za/@@practice/set_filter", params=[
+        self.post("http://qap.everythingmaths.co.za/@@practice/set_filter", params=[
             ['selected1', 'on'],
             ['selected2', 'on']],
             description="Post /@@practice/set_filter")
-        self.post("http://qa.everythingmaths.co.za/@@practice/submit_response", params=[
+        self.post("http://qap.everythingmaths.co.za/@@practice/submit_response", params=[
             ['question1a', ''],
             ['click1', 'Check']],
             description="Post /@@practice/submit_response")
-        self.post("http://qa.everythingmaths.co.za/@@practice/submit_response", params=[
+        self.post("http://qap.everythingmaths.co.za/@@practice/submit_response", params=[
             ['question1a', ''],
             ['nextPage', 'Go to next question']],
             description="Post /@@practice/submit_response")
-        self.post("http://qa.everythingmaths.co.za/@@practice/submit_response", params=[
+        self.post("http://qap.everythingmaths.co.za/@@practice/submit_response", params=[
             ['question1a', ''],
             ['click1', 'Check']],
             description="Post /@@practice/submit_response")
-        self.post("http://qa.everythingmaths.co.za/@@practice/submit_response", params=[
+        self.post("http://qap.everythingmaths.co.za/@@practice/submit_response", params=[
             ['question1a', ''],
             ['nextPage', 'Go to next question']],
             description="Post /@@practice/submit_response")
-        self.post("http://qa.everythingmaths.co.za/@@practice/submit_response", params=[
+        self.post("http://qap.everythingmaths.co.za/@@practice/submit_response", params=[
             ['question1a', ''],
             ['question1b', ''],
             ['click1', 'Check']],
             description="Post /@@practice/submit_response")
-        self.post("http://qa.everythingmaths.co.za/@@practice/submit_response", params=[
+        self.post("http://qap.everythingmaths.co.za/@@practice/submit_response", params=[
             ['question1a', ''],
             ['question1b', ''],
             ['nextPage', 'Go to next question']],
             description="Post /@@practice/submit_response")
-        self.post("http://qa.everythingmaths.co.za/@@practice/submit_response", params=[
+        self.post("http://qap.everythingmaths.co.za/@@practice/submit_response", params=[
             ['question1a', ''],
             ['click1', 'Check']],
             description="Post /@@practice/submit_response")
-        self.get("http://qa.everythingmaths.co.za/@@practice/question",
+        self.get("http://qap.everythingmaths.co.za/@@practice/question",
             description="Get /@@practice/question")
-        self.post("http://qa.everythingmaths.co.za/@@practice/submit_response", params=[
+        self.post("http://qap.everythingmaths.co.za/@@practice/submit_response", params=[
             ['question1a', ''],
             ['nextPage', 'Go to next question']],
             description="Post /@@practice/submit_response")
-        self.post("http://qa.everythingmaths.co.za/@@practice/submit_response", params=[
+        self.post("http://qap.everythingmaths.co.za/@@practice/submit_response", params=[
             ['question1a', ''],
             ['click1', 'Check']],
             description="Post /@@practice/submit_response")
-        self.post("http://qa.everythingmaths.co.za/@@practice/submit_response", params=[
+        self.post("http://qap.everythingmaths.co.za/@@practice/submit_response", params=[
             ['question1a', ''],
             ['nextPage', 'Go to next question']],
             description="Post /@@practice/submit_response")
-        self.post("http://qa.everythingmaths.co.za/@@practice/submit_response", params=[
-            ['question1a', ''],
-            ['question1b', ''],
-            ['click1', 'Check']],
-            description="Post /@@practice/submit_response")
-        self.post("http://qa.everythingmaths.co.za/@@practice/submit_response", params=[
-            ['question1a', ''],
-            ['question1b', ''],
-            ['nextPage', 'Go to next question']],
-            description="Post /@@practice/submit_response")
-        self.post("http://qa.everythingmaths.co.za/@@practice/submit_response", params=[
+        self.post("http://qap.everythingmaths.co.za/@@practice/submit_response", params=[
             ['question1a', ''],
             ['question1b', ''],
             ['click1', 'Check']],
             description="Post /@@practice/submit_response")
-        self.post("http://qa.everythingmaths.co.za/@@practice/submit_response", params=[
+        self.post("http://qap.everythingmaths.co.za/@@practice/submit_response", params=[
             ['question1a', ''],
             ['question1b', ''],
             ['nextPage', 'Go to next question']],
             description="Post /@@practice/submit_response")
-        self.post("http://qa.everythingmaths.co.za/@@practice/submit_response", params=[
+        self.post("http://qap.everythingmaths.co.za/@@practice/submit_response", params=[
             ['question1a', ''],
             ['question1b', ''],
             ['click1', 'Check']],
             description="Post /@@practice/submit_response")
-        self.post("http://qa.everythingmaths.co.za/@@practice/submit_response", params=[
+        self.post("http://qap.everythingmaths.co.za/@@practice/submit_response", params=[
             ['question1a', ''],
             ['question1b', ''],
             ['nextPage', 'Go to next question']],
             description="Post /@@practice/submit_response")
-        self.get("http://qa.everythingmaths.co.za/@@practice/dashboard",
+        self.post("http://qap.everythingmaths.co.za/@@practice/submit_response", params=[
+            ['question1a', ''],
+            ['question1b', ''],
+            ['click1', 'Check']],
+            description="Post /@@practice/submit_response")
+        self.post("http://qap.everythingmaths.co.za/@@practice/submit_response", params=[
+            ['question1a', ''],
+            ['question1b', ''],
+            ['nextPage', 'Go to next question']],
+            description="Post /@@practice/submit_response")
+        self.get("http://qap.everythingmaths.co.za/@@practice/dashboard",
             description="Get /@@practice/dashboard")
-        self.get("http://qa.everythingmaths.co.za/@@practice/chapter_vote/4?_=1335182470909",
+        self.get("http://qap.everythingmaths.co.za/@@practice/chapter_vote/4?_=1335182470909",
             description="Get /@@practice/chapter_vote/4")
 
         # end of test -----------------------------------------------
