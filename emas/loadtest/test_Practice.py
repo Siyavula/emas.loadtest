be783ecae4076c22a26ab32b4a3f42887db56b72
Rijk Stofberg <rijk@upfrontsystems.co.za>
2013-08-08 15:11:53 +0200

    Remove rubbish urls

--- emas/loadtest/test_Practice.py
+++ emas/loadtest/test_Practice.py
@@ -38,20 +38,6 @@ class Practice(FunkLoadTestCase):
         # /tmp/tmpCz5DHd_funkload/watch0050.request
         self.get("http://themes.googleusercontent.com/static/fonts/montserrat/v3/zhcz-_WihjSQC0oHJ9TCYBsxEYwM7FgeyaSgU71cLG0.woff",
             description="Get /static/fonts/monts...FgeyaSgU71cLG0.woff")
-        # /tmp/tmpCz5DHd_funkload/watch0086.request
-        self.post("http://safebrowsing.clients.google.com/safebrowsing/downloads?client=navclient-auto-ffox&appver=22.0&pver=2.2&wrkey=AKEgNitFmiyqCABfE19sA9qctohQ2Ca8tEbDi-xDcVzSqRmN5Sqw0ETDiSZ6nWuyes-ZzEH8LUs5gmnT_uAdABQOw053vA7uPg==", Data('text/plain', '''goog-phish-shavar;a:288853-294646:s:139003-143094:mac
-goog-malware-shavar;a:113422-122391:s:107521-118823:mac
-'''),
-            description="Post /safebrowsing/downloads")
-        # /tmp/tmpCz5DHd_funkload/watch0087.request
-        self.get("http://safebrowsing-cache.google.com/safebrowsing/rd/ChNnb29nLW1hbHdhcmUtc2hhdmFyEAAYjbwHIKC8ByoGGd4BAP8AMgYN3gEA_w8",
-            description="Get /safebrowsing/rd/Ch...Gd4BAP8AMgYN3gEA_w8")
-        # /tmp/tmpCz5DHd_funkload/watch0088.request
-        self.get("http://safebrowsing-cache.google.com/safebrowsing/rd/ChFnb29nLXBoaXNoLXNoYXZhchABGOndCCCQ3ggqCPguAgD___8BMgbpLgIA_38",
-            description="Get /safebrowsing/rd/Ch...AgD___8BMgbpLgIA_38")
-        # /tmp/tmpCz5DHd_funkload/watch0089.request
-        self.get("http://safebrowsing-cache.google.com/safebrowsing/rd/ChFnb29nLXBoaXNoLXNoYXZhchAAGPb9ESD6_REqBfh-BAAHMgX2fgQAAw",
-            description="Get /safebrowsing/rd/Ch...qBfh-BAAHMgX2fgQAAw")
         # /tmp/tmpCz5DHd_funkload/watch0090.request
         self.post(server_url + "/login_form", params=[
             ['came_from', 'http://qap.everythingmaths.co.za'],
@@ -70,9 +56,11 @@ goog-malware-shavar;a:113422-122391:s:107521-118823:mac
             ['__ac_password', '12345'],
             ['submit', 'Sign in']],
             description="Post /login_form")
+
         # /tmp/tmpCz5DHd_funkload/watch0101.request
         self.get(server_url + "/@@practice/dashboard",
             description="Get /@@practice/dashboard")
+
         # /tmp/tmpCz5DHd_funkload/watch0139.request
         self.get(server_url + "/@@practice/select_chapter/105",
             description="Get /@@practice/select_chapter/105")
@@ -93,7 +81,7 @@ goog-malware-shavar;a:113422-122391:s:107521-118823:mac
             ['question1a', '333333'],
             ['click1', 'Check answer']],
             description="Post /@@practice/submit_response")
-
+	
         # end of test -----------------------------------------------
 
     def tearDown(self):
